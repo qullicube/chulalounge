2cf21ceefb1396c10abf952943d5146263539c29
Poon Wu <qullicube@live.com>
2013-10-13 16:47:32 +0700

    Added app init

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+gem 'rails'
+
+group :development do
+	gem 'sqlite3'
+end
+
+group :production do
+	gem 'rails_12factor'
+	gem 'pg'
+end
+
+gem 'sass-rails'
+gem 'uglifier'
+
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder'
+gem 'twitter-bootstrap-rails'
+gem 'jquery-rails'
+gem 'jquery-ui-rails'
+gem 'designmodo-flatuipro-rails'
+gem 'protected_attributes'
+gem 'angularjs-rails'
+
+group :doc do
+	gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChulaLounge::Application.config.secret_key_base = 'aa8df2b5997e88f80966bb23cab4c31a1a1aaba1b4e475287ececfc99a73948c26221a6aa7f06c5b50e1b6bfe17548e34be7e2d17473634653ffd9b8079ac9d1'
