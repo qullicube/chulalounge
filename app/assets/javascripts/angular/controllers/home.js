acbd60b9c5d7961e2749b00a987027bb30b3b149
Poon Wu <qullicube@live.com>
2013-10-20 20:01:02 +0700

    Added basic home ui

---
+++ app/assets/javascripts/angular/controllers/home.js
@@ -0,0 +1,14 @@
+App.controller('PageIndex', ['$scope', function($scope){
+	$scope.catalogs = [
+		{name: "ISE"},
+		{name: "BBA"},
+		{name: "EBA"},
+		{name: "JIPP"},
+		{name: "BALAC"},
+		{name: "OTHERS"},
+		{name: "JIPP"},
+		{name: "BALAC"},
+		{name: "OTHERS"}
+	];
+
+}]);
