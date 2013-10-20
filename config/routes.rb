835f7591e9fed441291fff7f802eb2b0d0e12076
Poon Wu <qullicube@live.com>
2013-10-20 20:01:27 +0700

    Added index and courseindex route

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ ChulaLounge::Application.routes.draw do
 
   get "pages/index"
   get "pages/nav"  
+  get "pages/course_index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
