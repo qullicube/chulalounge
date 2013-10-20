f3430468786c9b4807e487b1bf1eef59cc58f47d
Poon Wu <qullicube@live.com>
2013-10-20 19:59:00 +0700

    Added resource services for invoking RESTful methods

--- app/assets/javascripts/angular/services/model.js
+++
@@ -1,6 +0,0 @@
-App.factory('Model', ['$resource'], function($resource) {
-			return function(model) {
-				var db = model.toLowerCase();
-				return $resource('/'+db+'s'); //Return resource according to Model given
-			};
-		});
